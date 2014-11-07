b956b730dcd14af4a19b1564d1bc424ae789580c
Devan Beitel <devan.beitel@gmail.com>
2014-11-07 10:32:39 -0600

    Attach users to posts and posts to users. Allow users to delete post if user created post.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -5,6 +5,8 @@ app.controller('NavCtrl', function NavCtrl($scope, $location, Post, Auth) {
     $scope.user = Auth.user;
 
     $scope.submitPost = function() {
+      $scope.post.creator = $scope.user.profile.username;
+      $scope.post.creatorUID = $scope.user.uid;
       Post.create($scope.post).then(function(ref){
           $location.path('/posts/' + ref.name());
           $scope.post = {url: 'http://', title: ''};
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,8 +1,8 @@
 'use strict';
 
-app.controller('PostsCtrl', function PostsCtrl($scope, $location, Post) {
+app.controller('PostsCtrl', function PostsCtrl($scope, $location, Post, Auth) {
     $scope.posts = Post.all;
-
+    $scope.user = Auth.user;
     $scope.post = {url: 'http://', title: ''};
 
     $scope.deletePost = function(post){
